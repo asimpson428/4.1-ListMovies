b1713c2c904a07f2c690a12a16435a87e94baefd
alexis <asimpson428@gmail.com>
2022-05-07 14:32:17 -0400

    babys first commit

---
+++ ListMovies/src/Movie.java
@@ -0,0 +1,34 @@
+
+public class Movie {
+	
+	public String title;
+	public String category;
+	public Movie(String title, String category) {
+		super();
+		this.title = title;
+		this.category = category;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+
+	}
+	
+
+
